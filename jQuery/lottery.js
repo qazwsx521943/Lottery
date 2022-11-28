c731e13b1600e6c88b6bb374c15ddaaca796922b
Chung Hsuan <qazwsx5219430630@gmail.com>
2022-11-28 20:57:28 +0800

    getRandom, showNums

--- jQuery/lottery.js
+++ jQuery/lottery.js
@@ -4,3 +4,34 @@ var popoverTriggerList = [].slice.call(
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 });
+// ⇧ Bootstrap plugin
+let randomSel = [];
+
+// random number function
+let getRandom = () => {
+  let i = Math.floor(Math.random() * 49 + 1);
+  if (randomSel.every((num) => num != i)) {
+    randomSel.push(i);
+  } else {
+    getRandom();
+  }
+};
+
+// Create five random numbers
+function fiveRandom() {
+  for (let i = 0; i < 5; i++) {
+    getRandom();
+  }
+}
+
+function showNums() {
+  let numArray = [];
+  let nums = document.querySelectorAll("input");
+  nums.forEach((x) => numArray.push(x.value));
+  fiveRandom();
+}
+
+$("#sub").click(showNums);
+$("#reset").click(function () {
+  randomSel = [];
+});
